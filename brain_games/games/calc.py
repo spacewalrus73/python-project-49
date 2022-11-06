c26abe67aebc1f6767b8ee41d9cc769a7e5c5da1
Daniil Balabanov <spacewalrus@bk.ru>
2022-11-06 06:11:47 +0300

    add new file

---
+++ brain_games/games/calc.py
@@ -0,0 +1,36 @@
+from brain_games.engine import main_actions
+from random import shuffle, randint
+
+
+def to_calc():
+    condition = 'What is the result of the expression?'
+    lst = []
+    answers = []
+    numbers_list = []
+    numbers_len = 6
+    operands = [' + ', ' - ', ' * ']
+    shuffle(operands)
+
+    for i in range(numbers_len):
+        numbers_list.append(randint(1, 100))
+
+    counter_for_operands = 0
+
+    for i, item in enumerate(numbers_list):
+        if i % 2 == 0:
+            first_char = str(item)
+            if operands[counter_for_operands].strip() == '+':
+                answers.append(str(item + numbers_list[i + 1]))
+            elif operands[counter_for_operands].strip() == '-':
+                answers.append(str(item - numbers_list[i + 1]))
+            else:
+                answers.append(str(item * numbers_list[i + 1]))
+        else:
+            lst.append(first_char + operands[counter_for_operands] + str(item))
+            counter_for_operands += 1
+    return condition, lst, answers
+
+
+def push():
+    condition, lst, answers = to_calc()
+    main_actions(condition, lst, answers)
---
+++ brain_games/scripts/script_for_calc.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from brain_games.games.calc import push
+
+
+def main():
+    push()
