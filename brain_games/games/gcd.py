d7cc8236cd48390faf2f144012b96f94f3140b5c
Daniil Balabanov <spacewalrus@bk.ru>
2022-11-07 19:47:41 +0300

    add new game

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,28 @@
+from random import randint
+from math import gcd
+from brain_games.engine import main_actions
+
+
+# Func append in lists correct answers and random numbers
+def to_find_gcd():
+    condition = 'Find the greatest common divisor of given numbers'
+    lst = []
+    answers = []
+    len_lists = 3
+    index = 1
+
+    while index <= len_lists:
+        number_1 = randint(1, 100)
+        number_2 = randint(1, 100)
+        answers.append(str(gcd(number_1, number_2)))
+        item = str(number_1) + ' ' + str(number_2)
+        lst.append(item)
+        index += 1
+
+    return condition, lst, answers
+
+
+# Func send data to func in module engine
+def push():
+    condition, lst, answers = to_find_gcd()
+    main_actions(condition, lst, answers)
