79b592a0b20c82c33e127450f1e5a1a94612ad8b
Daniil Balabanov <spacewalrus@bk.ru>
2022-11-07 22:13:13 +0300

    new files

---
+++ brain_games/games/progression.py
@@ -0,0 +1,33 @@
+from random import randint
+from brain_games.engine import main_actions
+
+
+# Func makes progressions, replace 1 symbol in it to '..',
+# and append this number to answers
+def making_progression():
+    condition = 'What number is missing in the progression?'
+    lst = []
+    answers = []
+    len_lists = 3
+    index = 0
+
+    while index < len_lists:
+        start = randint(1, 10)
+        stop = randint(25, 30)
+        step = randint(2, 4)
+        var_list = list(range(start, stop, step))
+        hidden_index = randint(0, len(var_list) - 1)
+        hidden_num = str(var_list[hidden_index])
+        var_list[hidden_index] = '..'
+        var_list = ' '.join(map(str, var_list))
+        lst.append(var_list)
+        answers.append(hidden_num)
+        index += 1
+
+    return condition, lst, answers
+
+
+# Func send data to func in module engine
+def push():
+    condition, lst, answers = making_progression()
+    main_actions(condition, lst, answers)
---
+++ brain_games/scripts/script_for_progress.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+from brain_games.games.progression import push
+
+
+def main():
+    push()
