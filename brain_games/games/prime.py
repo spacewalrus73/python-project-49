d936a88565ef52230df9d4fb255cb8c2271ff24c
Daniil Balabanov <spacewalrus@bk.ru>
2022-11-11 20:28:10 +0300

    new game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,23 @@
+from random import shuffle
+from sympy.ntheory import isprime
+from brain_games.engine import main_actions
+
+
+def is_prime():
+    condition = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+    lst = []
+    answers = []
+    numbers_list = list(range(2, 100))
+    shuffle(numbers_list)
+    for num in range(3):
+        lst.append(numbers_list[num])
+        if isprime(numbers_list[num]) is True:
+            answers.append('yes')
+        else:
+            answers.append('no')
+    return condition, lst, answers
+
+
+def push():
+    condition, lst, answers = is_prime()
+    main_actions(condition, lst, answers)
