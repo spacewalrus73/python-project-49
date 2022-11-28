bf6a02b3f66a60168174d6bf14ab5e494334dfb2
Daniil Balabanov <spacewalrus@bk.ru>
2022-11-28 11:40:19 +0300

    delete dir games_modules

--- brain_games/games_modules/calc_module.py
+++
@@ -1,8 +0,0 @@
-def to_calc(number_1, operand, number_2):
-    if operand == ' + ':
-        result = number_1 + number_2
-    elif operand == ' - ':
-        result = number_1 - number_2
-    else:
-        result = number_1 * number_2
-    return result
--- brain_games/games_modules/even_module.py
+++
@@ -1,5 +0,0 @@
-def is_even(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
